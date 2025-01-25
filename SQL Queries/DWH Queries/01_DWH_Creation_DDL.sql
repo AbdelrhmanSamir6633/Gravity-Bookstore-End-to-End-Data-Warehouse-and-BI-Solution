87560419ced23c54cc08cef125d56c2597456641
sarahfikry22 <112645119+sarahfikry22@users.noreply.github.com>
2025-01-25 19:01:59 +0200

    Add files via upload

---
+++ SQL Queries/DWH Queries/01_DWH_Creation_DDL.sql
@@ -0,0 +1,19 @@
+
+-- Check if there is a previous DWH that's name is Gravity_BookStore_DWH or not:
+
+IF EXISTS (
+		   SELECT 
+				name 
+           FROM 
+				sys.databases 
+           WHERE 
+				name = 'Gravity_BookStore_DWH'
+		  )
+DROP DATABASE Gravity_BookStore_DWH
+
+
+--Create a new DWH:
+
+CREATE DATABASE Gravity_BookStore_DWH
+
+
